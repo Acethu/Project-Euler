56fcf1f09b77190801f162463fa8e14d4d82c9e5
Acethu <Mhondra@t-online.de>
2019-09-06 23:28:10 +0200

    added basic_window.py

---
+++ basic_window.py
@@ -0,0 +1,24 @@
+# imports
+from PyCleanUI.Window import Window
+from PyCleanUI.widgets.Text import Text # text label to display a string of text
+from PyCleanUI.widgets.Button import Button # simple button with text
+from PyCleanUI.widgets.Input import Input # simple input
+# layout
+layout = [
+    Text("Fill in these boxes"),
+    Text("Name:"),
+    Input("", width=400), # width *optional
+    Text("Email:"),
+    Input("", width=400),
+    Button("Done")
+]
+# window
+window = Window("PyCleanUI Tutorial", layout)
+# main loop
+while window.is_running():
+    window.update() # always update the window before getting events
+    if window.get_event() is not None: # on event
+        print("event:", window.get_event()) # returns the button, that has been pressed
+        print("values:", window.get_values()) # returns values of the inputs
+
+window.close() # closes the window properly
